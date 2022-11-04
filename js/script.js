3f5ecb73c5397f0d8c93115c6af566ce0fe8d634
FR_Jess <fr_jess@icloud.com>
2022-11-04 12:49:29 +0100

    [add] tasks array in Vue

--- js/script.js
+++ js/script.js
@@ -4,4 +4,24 @@ const { createApp } = Vue;
 
 createApp({
 
+  data(){
+    return{
+      logoImage: 'img/logo.png',
+      tasks:[
+        {
+          name: 'Comprare il pane',
+          done: false,
+        },
+        {
+          name: 'Studiare Vue',
+          done: true,
+        },
+        {
+          name: 'Chiamare mamma',
+          done: false,
+        }
+      ]
+    }
+  }
+
 }).mount('#app')
