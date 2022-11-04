7e9c73173298524d36bdc54cb1f06913845c3dd6
FR_Jess <fr_jess@icloud.com>
2022-11-04 16:33:25 +0100

    [add] comments

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const { createApp } = Vue;
 
 createApp({
 
+  // MY OBJECTS
   data(){
     return{
       logoImage: 'img/logo.png',
@@ -26,26 +27,40 @@ createApp({
     }
   },
 
+  // MY FUNCTIION
   methods:{
+
+    //remove task done from tasks-area
     removeTask(index){
+      //empty error message
       this.errorMessage = '';
+
+      //check if task is completed
       if(this.tasks[index].done){
         this.tasks.splice(index,1);
+
+      //error message if task to remove is not completed
       }else{
-        // this.isAlertShow = this.errorMessage,
         this.errorMessage = 'Wait a minute...You have to complete this task before to remove it.';
-        // this.alertClass = ;
       }
     },
+
+    //create task to add to tasks-area
     createTask(){
+      //empty error message
       this.errorMessage = '';
+
+      //check if task name is more than 5 characters
       if(this.newTaskName.length < 5){
         this.errorMessage = 'Task name must have at least 5 characters.';
+      
+      //create a new task using input  
       }else{
         const newTask = {
           name: this.newTaskName,
           done: false
         }
+        //add new task to tasks-area
         this.tasks.unshift(newTask);
         this.newTaskName = '';
       }
