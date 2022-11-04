07f073461d9684eaeeb13e0f54ccd52218abf8f5
FR_Jess <fr_jess@icloud.com>
2022-11-04 10:22:39 +0100

    [add] html

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+//VUE
+const { createApp } = Vue;
+
+createApp({
+
+//   // MY OBJECTS
+  data(){
+    return{
+
+      
+  },
+
+//   // MY FUNCTIONS
+//   methods:{
+
+    
+
+
+//   mounted(){
+ 
+
+  }
+}).mount('#app');
